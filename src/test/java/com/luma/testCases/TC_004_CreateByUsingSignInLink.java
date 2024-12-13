dde5a011190e665be1995699bcfc5c311a0fdd03
sivakumar <msivakumar2702@gmail.com>
2024-12-13 12:41:29 +0530

    second time

--- src/test/java/com/luma/testCases/TC_004_CreateByUsingSignInLink.java
+++ src/test/java/com/luma/testCases/TC_004_CreateByUsingSignInLink.java
@@ -7,8 +7,6 @@ import com.luma.pageObjects.CreateNewCustomerAccountpage;
 import com.luma.pageObjects.CustomerLoginPage;
 import com.luma.pageObjects.HomePage;
 import com.luma.pageObjects.MyAccountPage;
-import com.luma.utilities.ExcelUtils;
-import com.luma.utilities.FilePaths;
 import com.luma.utilities.TestDataUtils;
 
 public class TC_004_CreateByUsingSignInLink extends BaseClass {
@@ -18,7 +16,7 @@ public class TC_004_CreateByUsingSignInLink extends BaseClass {
 	String email = TestDataUtils.generateEmail(firstName, lastName);
 	String pwd = TestDataUtils.password;
 
-	@Test(groups={"regression"})
+	@Test(groups = { "regression" })
 	public void createCusBysignin() {
 		HomePage hp = new HomePage(driver);
 
@@ -36,10 +34,10 @@ public class TC_004_CreateByUsingSignInLink extends BaseClass {
 		cnca.clickCreateAccountBtn();
 
 		// Set Excel file path
-		//ExcelUtils.setFilePath(FilePaths.CUSTOMER_DETAILS_EXCEL);
+		// ExcelUtils.setFilePath(FilePaths.CUSTOMER_DETAILS_EXCEL);
 
 		// Write customer data to Excel
-		//ExcelUtils.writeCustomerDataToExcel(firstName, lastName, email, pwd);
+		// ExcelUtils.writeCustomerDataToExcel(firstName, lastName, email, pwd);
 
 		// Logout
 		MyAccountPage ma = new MyAccountPage(driver);
---
+++ src/test/java/com/luma/testCases/TC_006_EndToEndTest.java
@@ -0,0 +1,5 @@
+package com.luma.testCases;
+
+public class TC_006_EndToEndTest {
+
+}
